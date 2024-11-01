84490e52a5f53e24c68bbfd74ea957f43f504e07
Tobi <streckel@gmx.de>
2024-11-01 21:04:06 +0100

    Update DropdownTreeWidget.php
    fix error that only the 1 level is shown

--- DropdownTreeWidget.php
+++ DropdownTreeWidget.php
@@ -1,132 +1,86 @@
 <?php
+
 namespace delikatesnsk\treedropdown;
 
 class DropdownTreeWidget extends \yii\base\Widget
 {
-    public $form = null; //ActiveForm
-    public $model = null; //model
-    public $attribute = null; //model attribute
-    public $multiSelect = 'auto'; //true, false or 'auto'
-    public $searchPanel = [ 'visible' => false, 
-                            'label' => '', //text before search input 
-                            'placeholder' => '',  //serch input placeholder text
-                            'searchCaseSensivity' => false 
-                          ];
+    public $form = null; // ActiveForm
+    public $model = null; // model
+    public $attribute = null; // model attribute
+    public $multiSelect = 'auto'; // true, false or 'auto'
+    public $searchPanel = [ 
+        'visible' => false, 
+        'label' => '', // text before search input 
+        'placeholder' => '',  // search input placeholder text
+        'searchCaseSensivity' => false 
+    ];
     public $rootNode = [
-                          'visible' => true,  
-                          'label' => 'Root' 
-                       ];
+        'visible' => true,  
+        'label' => 'Root' 
+    ];
 
-    public $expand = false; //expand dropdown tree after show
+    public $expand = false; // expand dropdown tree after show
     public $ajax = null;
-    //[
-    //  'onNodeExpand' => [
-    //                  'url' => '', URL for ajax request
-    //                  'method' => 'post', //post or get
-    //                  'params' => [
-    //                                  'param1' => 'value1',
-    //                                  'param2' => 'value1',
-    //                                  'param3' => 'value1',
-    //                                   ...
-    //                                  'paramN' => 'valueN',
-    //                                  'someparamName' => '%nodeId' // <-- %nodeId replaced to id of current node
-    //                              ]
-    //                  //returned Data will be array of array
-    //                  //[
-    //                  //  [
-    //                  //      'id' =>
-    //                  //      'label' =>
-    //                  //      'items' => [
-    //                  //                  [
-    //                  //                      'id' =>
-    //                  //                      'label' =>
-    //                  //                  ],
-    //                  //                  ... more
-    //                  //                 ]
-    //                  //  ],
-    //                  //  ... more
-    //                  //]
-    //                ],
-    //  'onNodeCollapse' => [
-    //                      ... see OnExpand, but the returned data will not be processed, only send ajax request
-    //                  ],
-    //]
-    public $label = false; //label of dropdown
-
-    public $items = null; //array of tree nodes with subnodes
+    public $label = false; // label of dropdown
+    public $items = null; // array of tree nodes with subnodes
 
     private $html = '';
     private $treeObject = null;
 
-    private function buildTreeObject( $items, &$parentItem = null ) {
-        if ( is_array( $items ) ) {
-            foreach ($items as $item) {
-                if ( is_array( $item ) && isset( $item['id'] ) && isset( $item['label'] ) ) {
-                    $node = new \stdClass();
-                    $node->parent = $parentItem;
-                    $node->id = $item['id'];
-                    $node->label = $item['label'];
-                    if ( isset( $item['items'] ) && is_array( $item['items'] ) && ( count( $item['items'] ) > 0 || $this->ajax !== null ) ) {
-                        $node->items = [];
-                        $this->buildTreeObject( $item['items'], $node );
-                    }
-                    $parentItem->items[] = $node;
+    private function buildTreeObject($items, &$parentItem) {
+        foreach ($items as $item) {
+            if (is_array($item) && isset($item['id']) && isset($item['label'])) {
+                $node = new \stdClass();
+                $node->id = $item['id'];
+                $node->label = $item['label'];
+                $node->items = []; // Initialize items for children
+                if (isset($item['items']) && is_array($item['items'])) {
+                    $this->buildTreeObject($item['items'], $node); // Recursion for children
                 }
+                // Attach the node to its parent's items
+                if (!isset($parentItem->items)) {
+                    $parentItem->items = [];
+                }
+                $parentItem->items[] = $node;
             }
         }
     }
 
-    public function buildTreeView( $items ) {
-        if ( is_array( $items ) && count( $items ) > 0 ) {
-            foreach( $items as $index => $item ) {
-                if (is_object( $item ) && isset( $item->id ) && isset( $item->label ) ) {
-                    if ( $index == 0 ) {
-                        //Если parent у item последний Node у своего parent добавляем класс last-node
-                        $class =  ( isset( $item->parent ) && $item->parent !== null && isset( $item->parent->parent ) && $item->parent->parent !== null && $item->parent->parent->items[ count( $item->parent->parent->items ) - 1] == $item->parent ?  " class=\"last-node\"" : "" );
-                        $this->html .= "<ul".$class.">\n";
-                    }
-
-                    $this->html .= "<li".( isset( $item->items ) && is_array( $item->items ) ? " class=\"parent\"" : "" ).">\n";
+    public function buildTreeView($items) {
+        if (is_array($items) && count($items) > 0) {
+            $this->html .= "<ul>\n"; // Start a new unordered list
+            foreach ($items as $item) {
+                if (is_object($item) && isset($item->id) && isset($item->label)) {
+                    $this->html .= "<li" . (isset($item->items) && count($item->items) > 0 ? " class=\"parent\"" : "") . ">\n";
                     $this->html .= "    <div class=\"node\">\n";
-                    $this->html .= "        ".( isset( $item->items ) && is_array( $item->items ) && ( count( $item->items ) > 0 || $this->ajax !== null ) ? "<i class=\"fa fa-plus-square-o\"></i>\n" : "" );
-                    $this->html .= "        ".( $this->multiSelect ? "<i class=\"fa fa-square-o\"></i>" : "" );
-                    $this->html .= "        <span".( ( isset( $item->id ) ? " data-id='".$item->id."'" : "" ) ).">".( isset( $item->label ) ? $item->label : "&nbsp;" )."</span>\n";
+                    $this->html .= "        <span data-id='" . $item->id . "'>" . $item->label . "</span>\n";
                     $this->html .= "    </div>\n";
-                    if ( isset( $item->items ) && is_array( $item->items ) && ( count( $item->items ) > 0 || $this->ajax !== null )  ) {
-                        $this->buildTreeView( $item->items );
+                    // Check for children and call recursively
+                    if (isset($item->items) && count($item->items) > 0) {
+                        $this->buildTreeView($item->items);
                     }
-                    $this->html .= "</li>\n";
-
-                    if ( $index == count( $items ) - 1 ) {
-                        $this->html .= "</ul>\n";
-                    }
+                    $this->html .= "</li>\n"; // Close the list item
                 }
             }
+            $this->html .= "</ul>\n"; // Close the unordered list
         }
     }
 
     public function init()
     {
         parent::init();
-        if ( !is_bool( $this->multiSelect ) ) {
-
-            $multiSelect = false;
-
-            if ( mb_convert_case( $this->multiSelect, MB_CASE_LOWER ) == 'auto'  ) {
-
-                if ( $this->form !== null && ( $this->form instanceof \yii\widgets\ActiveForm ) && $this->model !== null && $this->model[0] instanceof \yii\base\Model ) {
-                    $multiSelect = true;
-                }
-            }
-
-            $this->multiSelect = $multiSelect;
+        // Initialize multiSelect
+        if (!is_bool($this->multiSelect)) {
+            $this->multiSelect = mb_convert_case($this->multiSelect, MB_CASE_LOWER) === 'auto' && $this->form instanceof \yii\widgets\ActiveForm;
         }
+
+        // Initialize tree object with root
         $this->treeObject = new \stdClass();
         $this->treeObject->id = -1;
-        $this->treeObject->label = 'Root';
+        $this->treeObject->label = $this->rootNode['label'];
         $this->treeObject->items = [];
-        $this->buildTreeObject($this->items, $this->treeObject );
-        $this->buildTreeView( $this->treeObject->items );
+        $this->buildTreeObject($this->items, $this->treeObject);
+        $this->buildTreeView($this->treeObject->items);
     }
 
     public function run()
